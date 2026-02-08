265381d4d2ac4a940346a5432f9bb13caf44347e
Obafunso Ridwan <104316701+0xridwanobafunso@users.noreply.github.com>
2026-02-08 20:23:57 +0100

    changed relayers addrs (#6)
    * changed relayers addrs

--- scripts/safe/utils/getDeployment.ts
+++ scripts/safe/utils/getDeployment.ts
@@ -7,7 +7,7 @@ export const getDeployment = async () => {
   const SAFE_ADDRESS = process.env.SAFE_ADDRESS as string;
   const Handoff = await ethers.getContractFactory('Handoff');
   const args = {
-    relayers: ['0xFcDcD01BCaB08C9551Dd87eF552f3916F9875b12', '0xf3207f8BB31c9c27FaBebFFaeb69F060BfC37171'],
+    relayers: ['0xE863C8cf377927a27B364b3150f499FbB7595b49', '0xf7c15A0aB4Ce939a7482E2b23Fc79b1E3e95C430'],
     permit2: '0x000000000022D473030F116dDEE9F6B43aC78BA3',
     owner: SAFE_ADDRESS
   };
